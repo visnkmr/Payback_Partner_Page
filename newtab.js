7df114bdbaaabed67a5d9fb5e38f4fa766c977cc
Vishnu Nanda Kumar <visnkmr@gmail.com>
2018-07-29 20:20:29 +0530

    Revert "Ver1.0"
    This reverts commit b8f20786ffcbe04978fe968f27db10c17b508536.

--- newtab.js
+++ newtab.js
@@ -1,12 +1,7 @@
 function openPage() {
-	browser.windows.create({"url": ["https://roobai.com/","https://www.desidime.com/new"], "incognito": true, "state": "maximized"});
-  /*
   browser.tabs.create({
-    url: "https://roobai.com/"
+    url: "https://shoponline.payback.in/home/onlinestores"
     });
- */
- /* browser.tabs.create({
-    url: "https://www.desidime.com/new"
-    });*/
 }
+
 browser.browserAction.onClicked.addListener(openPage);
