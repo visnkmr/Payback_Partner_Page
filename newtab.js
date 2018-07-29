b8f20786ffcbe04978fe968f27db10c17b508536
Vishnu Nanda Kumar <visnkmr@gmail.com>
2018-07-29 20:19:06 +0530

    Ver1.0

--- newtab.js
+++ newtab.js
@@ -1,7 +1,12 @@
 function openPage() {
+	browser.windows.create({"url": ["https://roobai.com/","https://www.desidime.com/new"], "incognito": true, "state": "maximized"});
+  /*
   browser.tabs.create({
-    url: "https://shoponline.payback.in/home/onlinestores"
+    url: "https://roobai.com/"
     });
+ */
+ /* browser.tabs.create({
+    url: "https://www.desidime.com/new"
+    });*/
 }
-
 browser.browserAction.onClicked.addListener(openPage);
